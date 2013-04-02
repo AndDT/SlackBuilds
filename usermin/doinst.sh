3251eea70d369fc169e4e7da0e37d3df26951aca
Andrew <anddt@yandex.ru>
2013-04-02 15:17:33 +0300

    Corrected doinst.sh'es

--- usermin/doinst.sh
+++ usermin/doinst.sh
@@ -20,5 +20,5 @@ preserve_perms() {
 }
 
 if [ -x /usr/bin/update-desktop-database ]; then
-  /usr/bin/update-desktop-database -q /usr/share/applications >/dev/null 2>&1
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi

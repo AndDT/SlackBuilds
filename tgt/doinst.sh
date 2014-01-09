0993b4f64190f2960a532e92bf27a5344a18faa6
Andrew <anddt@yandex.ru>
2014-01-09 22:29:05 +0200

    Added tgt SlackBuild

---
+++ tgt/doinst.sh
@@ -0,0 +1,24 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+}
+
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+config etc/tgt/targets.conf.new
+preserve_perms etc/rc.d/rc.tgt.new

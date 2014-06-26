caca58104f76e3f184a116ae3033b893c9cb54bb
Andrew <anddt@yandex.ua>
2014-06-26 19:24:21 +0300

    Upgraded ede and edelib to 2.1

--- ede/doinst.sh
+++ ede/doinst.sh
@@ -17,5 +17,3 @@ if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
     /usr/bin/gtk-update-icon-cache usr/share/icons/hicolor >/dev/null 2>&1
   fi
 fi
-
-config etc/X11/xinit/xinitrc.ede.new
